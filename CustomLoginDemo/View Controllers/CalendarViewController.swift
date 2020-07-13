0c96a22c6eafba4fd00c226b855a8fea954df97f
sherry <s110519018@stu.ntue.edu.tw>
2020-07-13 23:24:47 +0800

    add alert 有bug

--- CustomLoginDemo/View Controllers/CalendarViewController.swift
+++ CustomLoginDemo/View Controllers/CalendarViewController.swift
@@ -9,8 +9,10 @@ import UIKit
 import FSCalendar
 
 class CalendarViewController: UIViewController,FSCalendarDelegate, FSCalendarDataSource {
+    //alert
+    let eventAlert = MyAlert()
+    //日曆
     var datesWithEvent = ["2020-07-05", "2020-07-06", "2020-07-12", "2020-07-25"]
-
     var datesWithMultipleEvents = ["2020-07-08", "2020-07-16", "2020-07-20", "2020-07-28"]
     @IBOutlet var calendar: FSCalendar!
     override func viewDidLoad() {
@@ -18,11 +20,6 @@ class CalendarViewController: UIViewController,FSCalendarDelegate, FSCalendarDat
         // Do any additional setup after loading the view.
         calendar.delegate = self
         calendar.dataSource = self
-        
-        calendar.select(Date())
-        calendar.scope = .week
-        calendar.accessibilityIdentifier = "calender"
-        
     }
  
     //設定日期格式
@@ -31,12 +28,21 @@ class CalendarViewController: UIViewController,FSCalendarDelegate, FSCalendarDat
         formatter.dateFormat = "yyyy-MM-dd"
         return formatter
     }()
+    //關閉alert
+    @objc func dismissAlert(){
+        eventAlert.dismissAlert()
+    }
     // 點了會印出日期
     func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
-        let formatter = DateFormatter()
-        formatter.dateFormat = "EEEE MM-dd-YYYY at h:mm a"
-        let string = formatter.string(from: date)
-        print("\(string)")
+//        let formatter = DateFormatter()
+//        formatter.dateFormat = "EEEE MM-dd-YYYY at h:mm a"
+//        let string = formatter.string(from: date)
+//        print("\(string)")
+        //點選跳出當天紀錄
+        let dateString = self.dateFormatter2.string(from: date)
+            eventAlert.showeventAlert(with: dateString,
+            message: "  ",
+            on: self)
     }
     //事件圖片
     func calendar(_ calendar: FSCalendar, imageFor date: Date) -> UIImage? {
@@ -53,11 +59,6 @@ class CalendarViewController: UIViewController,FSCalendarDelegate, FSCalendarDat
 
         return nil
     }
-//    func calendar(_ calendar: FSCalendar, boundingRectWillChange bounds: CGRect, animated: Bool) {
-//            self.calendarHeightConstraint.constant = bounds.height
-//            self.view.layoutIfNeeded()
-//    }
-    
 
 }
 
--- CustomLoginDemo/View Controllers/HomeViewController.swift
+++ CustomLoginDemo/View Controllers/HomeViewController.swift
@@ -11,7 +11,12 @@ import FirebaseAuth
 import Firebase
 
 class HomeViewController: UIViewController {
-
+    //alert
+    @IBOutlet var myButton: UIButton!
+    let customAlert = MyAlert()
+    var event_count = 6
+    var alert_event_count = " "
+    
     @IBOutlet weak var nicknameText: UILabel!
     
     @IBOutlet weak var logOutButton: UIButton!
@@ -41,8 +46,20 @@ class HomeViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         loadUserNickname()
+        //alert
+        myButton.backgroundColor = .link
+        myButton.setTitleColor(.white, for: .normal)
+        myButton.setTitle("新增事件", for: .normal)
     }
-    
+    //alert自動跳出
+    override func viewDidAppear(_ animated: Bool) {
+        alert_event_count = "今日事件量" + String(event_count) + "/6"
+        customAlert.showAlert(with: "請問要新增事件嗎？",
+        message: alert_event_count,
+        on: self)
+        event_count-=1
+        print(event_count)
+    }
     @IBAction func logOutTapped(_ sender: Any) {
         
         do {
@@ -57,5 +74,184 @@ class HomeViewController: UIViewController {
             print(error)
         }
     }
-    
+    //alert按按鈕跳出
+    @IBAction func didTapButton(){
+        customAlert.showAlert(with: "請問要新增事件嗎？",
+        message: alert_event_count,
+        on: self)
+    }
+    @objc func dismissAlert(){
+        customAlert.dismissAlert()
+    }
 }
+
+//alerts，其他的swift檔案也可以呼叫
+class MyAlert{
+    struct Constants {
+        static let backgroundAlphaTo: CGFloat = 0.6
+    }
+    //背景黑色
+    private let backgroundView: UIView = {
+       let backgroundView = UIView()
+        backgroundView.backgroundColor = .black
+        backgroundView.alpha = 0
+        return backgroundView
+    }()
+    //視窗
+    private let alertView: UIView = {
+        let alert = UIView()
+        alert.backgroundColor = .white
+        alert.layer.masksToBounds = true
+        alert.layer.cornerRadius = 12
+        return alert
+        
+    }()
+    private var mytargetView: UIView?
+    //新增事件視窗
+    func showAlert(with title: String,
+                   message: String,
+                   on ViewController: UIViewController){
+        guard let targetView = ViewController.view else{
+            return
+        }
+        
+        mytargetView = targetView
+        
+        backgroundView.frame = targetView.bounds
+        targetView.addSubview(backgroundView)
+        targetView.addSubview(alertView)
+        alertView.frame = CGRect(x: 40,
+                                 y: -300,
+                                 width: targetView.frame.size.width-80,
+                                 height: 300)
+        //標題
+        let titleLabel = UILabel(frame: CGRect(x: 0,
+                                               y: 0,
+                                               width: alertView.frame.size.width,
+                                               height: 80))
+        titleLabel.text = title
+        titleLabel.textAlignment = .center
+        alertView.addSubview(titleLabel)
+        //資訊
+        let messageLabel = UILabel(frame: CGRect(x: 0,
+                                               y: 80,
+                                               width: alertView.frame.size.width,
+                                               height: 170))
+        messageLabel.numberOfLines = 0
+        messageLabel.text = message
+        messageLabel.textAlignment = .center
+        alertView.addSubview(messageLabel)
+        //關閉按鈕
+        let button = UIButton(frame: CGRect(x: alertView.frame.size.width-(alertView.frame.size.width/2)-30,
+                                            y: 0,
+                                            width:alertView.frame.size.width,
+                                            height: 50))
+        button.setTitle("X", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.addTarget(self, action: #selector(dismissAlert),
+                         for: .touchUpInside)
+        alertView.addSubview(button)
+        //新增按鈕
+        let add_event = UIButton(frame: CGRect(x: alertView.frame.size.width/8,
+                                            y: alertView.frame.size.width-50,
+                                            width:alertView.frame.size.width-70,
+                                            height: 50))
+        add_event.setTitle("新增", for: .normal)
+        add_event.setTitleColor(.white, for: .normal)
+        add_event.backgroundColor = UIColor(red: 56/255, green: 83/255, blue: 143/255, alpha: 1)
+//        add_event.addTarget(self, action: #selector(dismissAlert),
+//                         for: .touchUpInside)
+        alertView.addSubview(add_event)
+        
+        UIView.animate(withDuration: 0.25,
+                       animations: {
+                        self.backgroundView.alpha = Constants.backgroundAlphaTo
+        },completion: { done in
+            if done{
+                UIView.animate(withDuration: 0.25,animations: {
+                    self.alertView.center = targetView.center
+                })
+            }
+        })
+    }
+    //過往事件視窗
+    func showeventAlert(with title: String,
+                       message: String,
+                       on ViewController: UIViewController){
+            guard let targetView = ViewController.view else{
+                return
+            }
+            
+            mytargetView = targetView
+            
+            backgroundView.frame = targetView.bounds
+            targetView.addSubview(backgroundView)
+            targetView.addSubview(alertView)
+            alertView.frame = CGRect(x: 40,
+                                     y: -300,
+                                     width: targetView.frame.size.width-80,
+                                     height: 300)
+            //標題
+            let titleLabel = UILabel(frame: CGRect(x: 0,
+                                                   y: 0,
+                                                   width: alertView.frame.size.width,
+                                                   height: 80))
+            titleLabel.text = title
+            titleLabel.textAlignment = .center
+            alertView.addSubview(titleLabel)
+            //資訊
+            let messageLabel = UILabel(frame: CGRect(x: 0,
+                                                   y: 80,
+                                                   width: alertView.frame.size.width,
+                                                   height: 170))
+            messageLabel.numberOfLines = 0
+            messageLabel.text = message
+            messageLabel.textAlignment = .center
+            alertView.addSubview(messageLabel)
+            //關閉按鈕
+            let button = UIButton(frame: CGRect(x: alertView.frame.size.width-(alertView.frame.size.width/2)-30,
+                                                y: 0,
+                                                width:alertView.frame.size.width,
+                                                height: 50))
+            button.setTitle("X", for: .normal)
+            button.setTitleColor(.black, for: .normal)
+            button.addTarget(self, action: #selector(dismissAlert),
+                             for: .touchUpInside)
+            alertView.addSubview(button)
+            UIView.animate(withDuration: 0.25,
+                           animations: {
+                            self.backgroundView.alpha = Constants.backgroundAlphaTo
+            },completion: { done in
+                if done{
+                    UIView.animate(withDuration: 0.25,animations: {
+                        self.alertView.center = targetView.center
+                    })
+                }
+            })
+        }
+    //關閉視窗動作
+    @objc func dismissAlert() {
+        guard let targetView = mytargetView else {
+            return
+        }
+        UIView.animate(withDuration: 0.25,
+                       animations: {
+                        self.alertView.frame = CGRect(x: 40,
+                                                      y: targetView.frame.size.height,
+                                                      width: targetView.frame.size.width-80,
+                                                      height: 300)
+        },completion: { done in
+            if done{
+                UIView.animate(withDuration: 0.25,animations: {
+                    self.backgroundView.alpha = 0
+                }, completion: { done in
+                    if done{
+                        self.alertView.removeFromSuperview()
+                        self.backgroundView.removeFromSuperview()
+                    }
+                })
+            }
+        })
+    }
+}
+
